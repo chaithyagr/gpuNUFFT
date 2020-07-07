a4c9263e1e8c64b4c5023357d3171c1cbcc825b0
chaithyagr <chaithyagr@gitlab.com>
2020-07-07 11:26:11 +0200

    Updates to code and add version

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="gpuNUFFT",
-    package_dir={"": "CUDA/bin"},
+    version="0.1.1a1",
     ext_modules=[
         CMakeExtension("gpuNUFFT", sourcedir=os.path.join("CUDA")),
     ],
